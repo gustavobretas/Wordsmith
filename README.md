517d74a1329d124f20e309586f6f96d7abbc8274
Gustavo Bretas <gustavobretas@gmail.com>
2022-04-28 13:14:42 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+## About Wordsmith
+  
+
+Project developed in accordance to the Course [Devdojo Wordsmith](https://devdojo.com/wordsmith/course)
---
+++ resources/views/components/logo.blade.php
@@ -0,0 +1,6 @@
+<svg {{ $attributes }} viewBox="0 0 202 120">
+    <defs></defs>
+    <g fill-rule="evenodd">
+        <path d="M0 0l69.245 120L96 73.633 53.51 0zM64 0l69.244 120L160 73.633 117.51 0zM127 0l37.5 65L202 0z"></path>
+    </g>
+</svg>
